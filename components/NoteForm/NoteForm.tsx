0ea512350e87b488601a879456b1c42c073dad43
Kitsuri <ki.tsu.ouji@gmail.com>
2026-06-19 20:11:26 +0300

    fix import

--- components/NoteForm/NoteForm.tsx
+++ components/NoteForm/NoteForm.tsx
@@ -2,7 +2,7 @@
 
 import css from "./NoteForm.module.css";
 import { useId, useState } from "react";
-import { createNote } from "@/lib/api";
+import { createNote } from "@/lib/api/clientApi";
 import { useMutation, useQueryClient } from "@tanstack/react-query";
 import { useRouter } from "next/navigation";
 import useNoteStore from "@/lib/store/noteStore";
